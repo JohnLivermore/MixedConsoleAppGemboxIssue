23c1f954046a103936d172c45cf4f6afd7ee0c42
John Livermore <jlivermore@bigwavesoftware.com>
2020-05-05 17:25:22 -0500

    1

---
+++ Library/GeneratedDocumentManager.cs
@@ -0,0 +1,17 @@
+﻿using GemBox.Document;
+using GemBox.Document.MailMerging;
+using System;
+using System.IO;
+using System.Linq;
+
+
+namespace Library
+{
+    public class GeneratedDocumentManager
+    {
+        public void Merge(DocumentModel word, object model)
+        {
+            word.MailMerge.Execute(model);
+        }
+    }
+}
---
+++ MixedConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using GemBox.Document;
+using Library;
+using System.IO;
+
+namespace MixedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = new DocumentModel();
+
+            var gdmgr = new GeneratedDocumentManager();
+            gdmgr.Merge(word, null);
+        }
+    }
+}
